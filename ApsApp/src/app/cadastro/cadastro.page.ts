1639a49ab87c4ac6f99b78ab07f8d2732875241a
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-11-01 13:21:09 -0300

    Feat: create user data

--- ApsApp/src/app/cadastro/cadastro.page.ts
+++ ApsApp/src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Iuser {
+  nome: string;
+  sobrenome: string;
+  email: string;
+  senha: string;
+}
+
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.page.html',
@@ -11,5 +18,36 @@ export class CadastroPage implements OnInit {
 
   ngOnInit() {
   }
+
+    public user: Iuser = {
+      nome: '',
+      sobrenome: '',
+      email: '',
+      senha: '',
+    };
+
+    username: string = ''
+    userLastName: string = ''
+    email: string = ''
+    password: string = ''
+
+    cadastrar(): void{
+      this.user.nome = this.username;
+      this.user.sobrenome = this.userLastName;
+      this.user.email = this.email;
+      this.user.senha = this.password;
+
+      if(this.user.nome == ''){
+        alert("Favor preencher o seu nome!")
+      }else if(this.user.sobrenome == ''){
+        alert("Favor preencher o seu sobrenome!")
+      }else if(this.user.email == ''){
+        alert("Favor preencher o seu email!")
+      }else if(this.user.senha == ''){
+        alert("Favor preencher a sua senha!")
+      }else {
+        alert('Cadastro concluido')
+      }
+    }
 
 }
