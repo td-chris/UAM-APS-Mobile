b4fcdda381a5314382afce12b2bfcbb88f690c2b
Christian Todesquine Dias <todesquinechristian@gmail.com>
2021-11-02 18:09:40 -0300

    Feat: create network content

--- moovieApp/src/app/network/network.page.ts
+++ moovieApp/src/app/network/network.page.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['network.page.scss']
 })
 export class NetworkPage {
+  public objeto_feed = {
+    nome: "Marty McFly",
+    data: "November 5, 1955",
+    descricao: "Wait a minute. Wait a minute, Doc ...",
+    qntd_likes: 12,
+    qntd_comments: 4,
+    tempo_comment: "11h ago"
+  }
 
   constructor() {}
 
